e409e79cb93b720f9372152ab245fd8097b54915
nbshig <nbshimorizo@gmail.com>
2019-02-17 22:50:11 +0900

    Add: LogCheck.ps と正規表現検証.txt の新規追加。
    ビルドバッチ実行後のログチェックを想定したスクリプト。
    ログチェック時、「1行の中に数値がある、かつ20以外の数字である」というちゅしゅつ条件を想定、
    かつ、その検証の為のテキストを追加。

---
+++ LogCheck.ps1
@@ -0,0 +1,13 @@
+﻿Get-ExecutionPolicy
+#Set-ExecutionPolicy RemoteSigned
+
+$arr_str行 = Get-Content C:\work\powerShellテスト\正規表現検証.txt -Encoding string | Select-Object -last 2
+
+foreach ($i in $arr_str行) {
+    Write-Host $i
+
+    if ($i -match "^(?=.*\d)(?!.*20).*$") {
+        #1行の中に数値がある、かつ、20以外である数値である場合
+        Write-Host "マッチした!"
+    }
+}

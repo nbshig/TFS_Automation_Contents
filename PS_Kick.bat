2aba08b7a76e65251ce2377c1740dad2d05bf14a
nbshig <nbshimorizo@gmail.com>
2019-02-17 21:03:42 +0900

    Improve: PowerShellコール時に引数を与える＆戻り値取得
    リポジトリ名を引数として与える事で、PowerShellスクリプトを共通関数として利用可能となる。
    戻り値を取得することでスクリプト結果を確認可能となる。

--- PS_Kick.bat
+++ PS_Kick.bat
@@ -1,11 +1,11 @@
 echo off
 echo PS_Kick.batを実行しています…
 
+rem powershell -ExecutionPolicy RemoteSigned %~dp0\test1.ps1
+powershell -ExecutionPolicy RemoteSigned "C:\work\powerShellテスト\work\Copy.ps1 %1;exit $LASTEXITCODE"
 
-rem powershell -ExecutionPolicy RemoteSigned C:\work\powerShellテスト\test2.ps1
-powershell -ExecutionPolicy RemoteSigned %~dp0\test1.ps1
-
+echo Powershellから受け取った戻り値→%ERRORLEVEL%
 
 echo 完了しました！
 pause > nul
-exit
+rem exit

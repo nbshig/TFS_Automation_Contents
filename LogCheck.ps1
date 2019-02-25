85d315bf42d064806ad403ffc02cafd021e90599
nbshig <nbshimorizo@gmail.com>
2019-02-25 22:09:43 +0900

    Improve:読み込みファイル後ろから20行分に変更＆ログファイル対象文言チェックに修正

--- LogCheck.ps1
+++ LogCheck.ps1
@@ -1,13 +1,19 @@
 ﻿Get-ExecutionPolicy
 #Set-ExecutionPolicy RemoteSigned
 
-$arr_str行 = Get-Content C:\work\powerShellテスト\正規表現検証.txt -Encoding string | Select-Object -last 2
+$arr_str行 = Get-Content C:\work\powerShellテスト\正規表現検証.txt -Encoding string | Select-Object -last 20
 
 foreach ($i in $arr_str行) {
     Write-Host $i
 
-    if ($i -match "^(?=.*\d)(?!.*20).*$") {
+    #if ($i -match "^(?=.*\d)(?!.*20).*$") {
         #1行の中に数値がある、かつ、20以外である数値である場合
-        Write-Host "マッチした!"
+    if ($i -match ("0 失敗|プリコンパイル処理終了|ビルドスクリプト処理正常終了")) {
+        Write-Host "正常終了!"
+        exit 0
     }
 }
+
+#最終行まで到達した場合、正常終了ログを発見出来なかったという事で、exit 1を返す
+Write-Host "最終行まで到達！"
+exit 1
